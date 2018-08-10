684f6c2693fbb885cf94fca872f8a3eab7fe0412
Blauto <Blauto@DESKTOP-AJEVJGM>
2018-08-10 15:13:37 +0200

    add favico

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE>
+<html>
+
+  <head>
+    <meta charset="utf-8"/>
+    <title>Upenglish</title>
+    <meta name="discription" content="nauka języka"/>
+    <meta name="keywords" content="angielski nauka kursy szkolenia e-learning kolonie"/>
+    <meta http-eguip="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="favicon.ico" />
+    
+    <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+    
+    <link rel="stylesheet" href="css/main.css" type="text/css"/>
+
+    <script src="js/" type="text/javascript"></script>
+  </head>
+
+  <body>
+    
+
+      
+  </body>
+</html>

234da728f72b3ab930028f1bee075c19cf2c2870
Blauto <Blauto@DESKTOP-AJEVJGM>
2018-08-12 22:12:46 +0200

    Add facebook tabSlide

--- index.php
+++ index.php
@@ -9,17 +9,30 @@
     <meta http-eguip="X-UA-Compatible" content="IE=edge,chrome=1"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="favicon.ico" />
-    
-    <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
-    
-    <link rel="stylesheet" href="css/main.css" type="text/css"/>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:500" rel="stylesheet">
 
-    <script src="js/" type="text/javascript"></script>
+    <link rel="stylesheet" href="css/main.css" type="text/css">
+    <link rel="stylesheet" href="css/jquery.tabSlideOut.css" type="text/css">
+
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script src="js/tabSlideOut/behavior.js"></script> 
   </head>
 
   <body>
     
+    <script src="js/tabSlideOut/jquery.tabSlideOut.js"></script>
+    <div id="my-tab">
 
-      
+      <span class="handle">Facebook</span>
+      <iframe 
+        src="https://www.facebook.com/plugins/page.php?
+        href=https%3A%2F%2Fwww.facebook.com%2Fupenglishpk%2F&tabs=timeline&width=280px&height=500&small_header=true&
+        adapt_container_width=true&hide_cover=true&show_facepile=true&appId" width="280px" height="500" 
+        style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media">
+        </iframe>
+
+    </div>
+
   </body>
+
 </html>
---
+++ js/tabSlideOut/behavior.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function() {
+    $('#my-tab').tabSlideOut( {
+        tabLocation: 'right',
+        action: 'click',  //not need
+        offset: '140px'
+    } );
+});

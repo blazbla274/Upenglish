b62458b4ca666bb0e271e6530b3e0eb5da05979a
Blauto <Blauto@DESKTOP-AJEVJGM>
2018-08-12 23:34:51 +0200

    Add and style google map

--- index.php
+++ index.php
@@ -16,20 +16,26 @@
 
     <script src="http://code.jquery.com/jquery-latest.min.js"></script>
     <script src="js/tabSlideOut/behavior.js"></script> 
+    <script src="js/initMap.js"></script>
   </head>
 
   <body>
     
+    <div id="map"></div>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCj_5EdnjB59acFe5ASNvgOmtDTI905eBE&callback=initMap">
+    </script>
+
     <script src="js/tabSlideOut/jquery.tabSlideOut.js"></script>
     <div id="my-tab">
 
       <span class="handle">Facebook</span>
       <iframe 
-        src="https://www.facebook.com/plugins/page.php?
-        href=https%3A%2F%2Fwww.facebook.com%2Fupenglishpk%2F&tabs=timeline&width=280px&height=500&small_header=true&
-        adapt_container_width=true&hide_cover=true&show_facepile=true&appId" width="280px" height="500" 
-        style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media">
-        </iframe>
+        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fupenglishpk%2F&tabs=timeline&
+        width=280px&height=500&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=true&appId" 
+        width="280px" height="500" style="border:none;overflow:hidden" scrolling="no" 
+        frameborder="0" allowTransparency="true" allow="encrypted-media">
+      </iframe>
 
     </div>
 
---
+++ js/initMap.js
@@ -0,0 +1,183 @@
+var styles = [
+        {
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#242f3e"
+            }
+          ]
+        },
+        {
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#746855"
+            }
+          ]
+        },
+        {
+          "elementType": "labels.text.stroke",
+          "stylers": [
+            {
+              "color": "#242f3e"
+            }
+          ]
+        },
+        {
+          "featureType": "administrative.locality",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#d59563"
+            }
+          ]
+        },
+        {
+          "featureType": "poi",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#d59563"
+            }
+          ]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#263c3f"
+            }
+          ]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#6b9a76"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#38414e"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "geometry.stroke",
+          "stylers": [
+            {
+              "color": "#212a37"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#9ca5b3"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#746855"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "geometry.stroke",
+          "stylers": [
+            {
+              "color": "#1f2835"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#f3d19c"
+            }
+          ]
+        },
+        {
+          "featureType": "transit",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#2f3948"
+            }
+          ]
+        },
+        {
+          "featureType": "transit.station",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#d59563"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#17263c"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#515c6d"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "labels.text.stroke",
+          "stylers": [
+            {
+              "color": "#17263c"
+            }
+          ]
+        }
+]
+
+function initMap() {
+    var nightMapType = new google.maps.StyledMapType(
+        styles,
+        { name: 'Noc' }
+    );
+    var location = {lat: 50.216341, lng: 19.157471};
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 12,
+        center: location,
+        mapTypeControlOptions: {
+            mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain', 'night_map']
+        }
+    });
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
+
+    map.mapTypes.set('night_map', nightMapType);
+    map.setMapTypeId('night_map');
+}

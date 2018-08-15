580d981fed0f333c66265d5aa164aa7b30a5bbbc
Blauto <Blauto@DESKTOP-AJEVJGM>
2018-08-15 14:35:37 +0200

    Init slider

---
+++ js/slider.js
@@ -0,0 +1,48 @@
+var carouselIndex = 0;
+var numberOfCells = 9;
+var zTranslate = 0;
+var resizing = true;
+$(document).ready(function() {
+    sliderResize();
+    setInterval(rotateCarousel, 3000);
+    $(window).resize(function() {
+      resizing = true;
+      $('.scene_resizing').css("opacity", "1");
+      sliderResize();
+    });
+});
+
+const sliderResize = () => {
+
+    let cellSize = parseInt($('.scene').css('width'));
+    let tz = Math.round( ( cellSize / 2 ) /  Math.tan( Math.PI / numberOfCells ) );
+    $('.cell:nth-child(1)').css( 'transform', 'rotateY(  0deg) translateZ('+tz+'px)');
+    $('.cell:nth-child(2)').css( 'transform', 'rotateY( 40deg) translateZ('+tz+'px)'); 
+    $('.cell:nth-child(3)').css( 'transform', 'rotateY( 80deg) translateZ('+tz+'px)'); 
+    $('.cell:nth-child(4)').css( 'transform', 'rotateY(120deg) translateZ('+tz+'px)'); 
+    $('.cell:nth-child(5)').css( 'transform', 'rotateY(160deg) translateZ('+tz+'px)'); 
+    $('.cell:nth-child(6)').css( 'transform', 'rotateY(200deg) translateZ('+tz+'px)'); 
+    $('.cell:nth-child(7)').css( 'transform', 'rotateY(240deg) translateZ('+tz+'px)'); 
+    $('.cell:nth-child(8)').css( 'transform', 'rotateY(280deg) translateZ('+tz+'px)'); 
+    $('.cell:nth-child(9)').css( 'transform', 'rotateY(320deg) translateZ('+tz+'px)'); 
+    zTranslate = tz;
+}
+
+const rotateCarousel = () => {
+    carouselIndex++;
+    let angle = carouselIndex / numberOfCells * -360;
+    document.querySelector('.carousel').style.transform = 'translateZ(-'+zTranslate+'px) rotateY(' + angle + 'deg)';
+
+    if(resizing) resizing = false;
+    else resizeOffDelay(200);
+      
+}
+
+async function resizeOffDelay(x) {
+    await sleep(x);
+    $('.scene_resizing').css("opacity", "0");
+} 
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}

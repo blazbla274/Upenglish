43ed4f10d15011a11f5bb2b80dce26ae50ba1d21
Blauto <Blauto@DESKTOP-AJEVJGM>
2018-08-18 11:41:30 +0200

    Sticky Nav

---
+++ js/nav.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    navSet();
+    $(window).scroll(function() {
+        navSet();
+    });
+});
+
+const navSet = () => {
+
+    if($(window).scrollTop() > 110 &&
+    $(window).scrollTop() < 250) {
+      $('nav > div').css(
+        { opacity: '0' },
+      );
+    } else {
+      $('nav > div').css(
+        { opacity: '1' },
+      );
+    }
+
+    if($(window).scrollTop() > 250) {
+      $('nav > div').css(
+        { position: 'fixed' },
+        { top: '0px' },
+        { left: '0px' }    
+      );  
+      $('nav > div').css({ height: '70px' });
+      $('.logo').css({height: '70px'});
+      } else {
+      $('nav > div').css(
+        { position: 'relative' },
+        { top: 'none' },
+        { left: 'none' }
+      );    
+      $('nav > div').css({ height: '100px' });
+      $('.logo').css({height: '90px'});
+    }
+}
